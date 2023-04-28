81fd904d50c96efc437a407f4253cc502ced92c7
tbalint19 <tothbalint719@gmail.com>
2023-04-28 20:42:36 +0200

    first working business logic (replace e to a)

--- index.ts
+++ index.ts
@@ -1,15 +1,26 @@
-import filesystem from "fs"
-let userInput: string = filesystem.readFileSync("./input.txt", "utf-8")
+import filesystem from "fs";
+let userInput: string = filesystem.readFileSync("./input.txt", "utf-8");
 //let fruit1: string = "cseresznye"
 
-let output: string = ""
+let output: string = "";
 
+let shouldStop: boolean = false;
 
-if (userInput !== "fuck")
-{
-  output = userInput
+for (let roundNumber = 1; shouldStop === false; roundNumber++) {
+  let character = userInput[roundNumber - 1]
+
+  if (character !== undefined) {
+    if (character === "e") {
+      output += "a"
+    } else {
+      output += character
+    }
+  }
+
+  if (character === undefined) {
+    shouldStop = true;
+  } 
 }
 
-
 //console.log(fruit1)
-filesystem.writeFileSync("./output.txt", output, "utf-8")
+filesystem.writeFileSync("./output.txt", output, "utf-8");
