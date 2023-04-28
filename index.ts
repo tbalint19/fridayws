185ea5b2b6a13c61f742c629c206d5b2a14c24ef
tbalint19 <tothbalint719@gmail.com>
2023-04-28 19:28:16 +0200

    working for loop

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world")
+import filesystem from "fs"
+let fruit1: string = filesystem.readFileSync("./input.txt", "utf-8")
+//let fruit1: string = "cseresznye"
+
+
+
+
+for (let roundNumber = 1; roundNumber <= 300; roundNumber++)
+{
+  fruit1 += "!"
+}
+
+
+//console.log(fruit1)
+filesystem.writeFileSync("./output.txt", fruit1, "utf-8")
+
+
+
+
+
